92c9424411dd45ee251ee6a17c270c4685083db5
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-08-17 07:49:04 +0530

    Creating a new cab invoice generator program

---
+++ CabInvoiceGeneratorProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CabInvoiceGeneratorProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Cab Invoice Generator");
+        }
+    }
+}
