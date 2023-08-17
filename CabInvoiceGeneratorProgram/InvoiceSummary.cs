fd799713070b4b208f6d1dd4dbdf396ba08c3244
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-08-17 08:27:50 +0530

    Createw a program for calculating multiple fare average fare

---
+++ CabInvoiceGeneratorProgram/InvoiceSummary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGeneratorProgram
+{
+    public class InvoiceSummary
+    {
+        public double totalFair;
+        public int numberOfRides;
+        public double average;
+        public InvoiceSummary(double totalFair, int numberOfRides)
+        {
+            this.totalFair = totalFair;
+            this.numberOfRides = numberOfRides;
+            this.average = totalFair / numberOfRides;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            else if (!(obj is InvoiceSummary))
+            {
+                return false;
+            }
+            else
+            {
+                InvoiceSummary invoiceSummary = (InvoiceSummary)obj;
+                return invoiceSummary.totalFair == totalFair && invoiceSummary.numberOfRides == numberOfRides;
+            }
+
+        }
+    }
+}
